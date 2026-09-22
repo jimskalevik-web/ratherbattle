5e6c808fa65a898757249e4a934503ad14ffcdc9
jimskalevik-web <jim.skalevik@gmail.com>
2026-09-22 07:34:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // RatherBattle service worker.
 // Nett først, alltid: spillet skal aldri vise en gammel versjon når det finnes
 // en ny. Lagret kopi brukes bare når telefonen er uten nett.
-const LAGER = "rb-2026-09-21 16:34 · ingen-gjentak+temaer+puls";
+const LAGER = "rb-2026-09-22 05:29 · ingen-gjentak+temaer+puls";
 self.addEventListener("install", (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(LAGER).then((c) => c.addAll(["/"])).catch(() => {}));
